a739731cca0301973ce31602eb8e0fdeade745be
Dai Suan Mung <daisuanmung8@gmail.com>
2022-01-13 23:31:56 +0100

    push images to azure registry

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=> res.send("Welcome to Docker Hub Image Registry"))
+app.get("/",(req,res)=> res.send("Welcome to Node Actions in Azure"))
 
 app.listen(3000, ()=>{
     console.log("my port run in 3000")
