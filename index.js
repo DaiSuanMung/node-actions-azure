3d37e76edb9e51b81fd2c4098e3004bfa76f5ce6
Dai Suan Mung <daisuanmung8@gmail.com>
2022-01-13 22:35:54 +0100

    connect with docker io

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=> res.send("Welcome from Azure Node Docker with Actions"))
+app.get("/",(req,res)=> res.send("Welcome from Azure Node Docker with Actions Container"))
 
 app.listen(3000, ()=>{
     console.log("my port run in 3000")
