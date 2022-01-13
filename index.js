b3c75f5d383bb6b2743330c0825028ea522fa470
Dai Suan Mung <daisuanmung8@gmail.com>
2022-01-13 22:56:33 +0100

    edit title

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=> res.send("Welcome from Azure Node Docker with Actions Container"))
+app.get("/",(req,res)=> res.send("Welcome to Docker Hub Image Registry"))
 
 app.listen(3000, ()=>{
     console.log("my port run in 3000")
