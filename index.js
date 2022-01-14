42c1c362a108c15ba2ce243f6406a9a16976cc81
Dai Suan Mung <daisuanmung8@gmail.com>
2022-01-14 01:31:26 +0100

    edit port

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.get("/",(req,res)=> res.send("Welcome to Node Actions in Azure"))
 
-app.listen(3000, ()=>{
+app.listen(80, ()=>{
     console.log("my port run in 3000")
 })
